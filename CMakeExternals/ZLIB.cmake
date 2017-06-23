68b235faf9746daa24f85cc6ff074ab0fc3d4b9a
sergey-denisov <s.s.denisov@yandex.ru>
2017-06-23 16:25:59 +0400

    add zlib as external project
    AUT-2343

---
+++ CMakeExternals/ZLIB.cmake
@@ -0,0 +1,65 @@
+#------------------------------------------------------------------
+# ZLIB
+#------------------------------------------------------------------
+  set(proj ZLIB)
+  set(proj_DEPENDENCIES )
+  set(ZLIB_DEPENDS ${proj})
+
+  if(ZLIB_FOUND)
+  else(ZLIB_FOUND)
+    set(additional_cmake_args )
+    if(CTEST_USE_LAUNCHERS)
+      list(APPEND additional_cmake_args
+        "-DCMAKE_PROJECT_${proj}_INCLUDE:FILEPATH=${CMAKE_ROOT}/Modules/CTestUseLaunchers.cmake"
+      )
+    endif()
+
+    ExternalProject_Add(${proj}
+      LIST_SEPARATOR ${sep}
+      GIT_REPOSITORY "https://github.com/madler/zlib.git"
+      GIT_TAG "v1.2.11"
+      CMAKE_ARGS
+        ${ep_common_args}
+        ${additional_cmake_args}
+      CMAKE_CACHE_ARGS
+        ${ep_common_cache_args}
+        -DBUILD_SHARED_LIBS:BOOL=OFF
+        -DZLIB_MANGLE_PREFIX:STRING=mitk_zlib_
+        -DZLIB_INSTALL_INCLUDE_DIR:STRING=include/mitk_zlib
+      CMAKE_CACHE_DEFAULT_ARGS
+        ${ep_common_cache_default_args}
+      DEPENDS ${proj_DEPENDENCIES}
+      )
+    set(ZLIB_DIR ${ep_prefix})
+    set(ZLIB_INCLUDE_DIR ${ZLIB_DIR}/include/mitk_zlib)
+
+    install(DIRECTORY ${ZLIB_INCLUDE_DIR}
+            DESTINATION include
+            COMPONENT dev)
+
+    find_library(ZLIB_LIBRARY_RELEASE NAMES zlib
+                 PATHS ${ZLIB_DIR}
+                 PATH_SUFFIXES lib lib/Release
+                 NO_DEFAULT_PATH)
+    find_library(ZLIB_LIBRARY_DEBUG NAMES zlibd
+                 PATHS ${ZLIB_DIR}
+                 PATH_SUFFIXES lib lib/Debug
+                 NO_DEFAULT_PATH)
+
+    set(ZLIB_LIBRARY )
+    if(ZLIB_LIBRARY_RELEASE)
+      list(APPEND ZLIB_LIBRARY optimized ${ZLIB_LIBRARY_RELEASE})
+      install(FILES ${ZLIB_LIBRARY_RELEASE}
+              DESTINATION lib
+              CONFIGURATIONS Release
+              COMPONENT dev)
+    endif()
+    if(ZLIB_LIBRARY_DEBUG)
+      list(APPEND ZLIB_LIBRARY debug ${ZLIB_LIBRARY_DEBUG})
+      install(FILES ${ZLIB_LIBRARY_DEBUG}
+              DESTINATION lib
+              CONFIGURATIONS Debug
+              COMPONENT dev)
+    endif()
+  endif(ZLIB_FOUND)
+
---
+++ SuperBuild.cmake
@@ -0,0 +1,182 @@
+
+#-----------------------------------------------------------------------------
+# ExternalProjects
+#-----------------------------------------------------------------------------
+
+set(external_projects
+  ZLIB
+  )
+
+#-----------------------------------------------------------------------------
+# External project settings
+#-----------------------------------------------------------------------------
+
+include(ExternalProject)
+
+set(ep_prefix "${CMAKE_BINARY_DIR}/ep")
+set_property(DIRECTORY PROPERTY EP_PREFIX ${ep_prefix})
+
+# It makes these directories for downloaded dependencies (GMP, MPFR)
+file(MAKE_DIRECTORY ${ep_prefix}/bin)
+file(MAKE_DIRECTORY ${ep_prefix}/lib)
+file(MAKE_DIRECTORY ${ep_prefix}/include)
+
+set(ep_install_dir ${ep_prefix})
+set(ep_suffix "-cmake")
+set(ep_build_shared_libs ON)
+set(ep_build_testing OFF)
+
+# Compute -G arg for configuring external projects with the same CMake generator:
+if(CMAKE_EXTRA_GENERATOR)
+  set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
+else()
+  set(gen "${CMAKE_GENERATOR}")
+endif()
+
+# Use this value where semi-colons are needed in ep_add args:
+set(sep "^^")
+
+##  Explicit setting of Windows version
+
+if(WIN32)
+  # for the best compatibility, always set Windows version to XP
+  set(ver "0x501")
+
+  # this method adds the necessary compiler flag
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_WIN32_WINNT=${ver}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WIN32_WINNT=${ver}")
+  # this adds a preprocessor definition to the project
+  add_definitions(-D_WIN32_WINNT="${ver}")
+endif()
+
+##  Explicit setting of Unix version
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+endif()
+
+##
+
+set(ep_common_C_FLAGS "${CMAKE_C_FLAGS}")
+set(ep_common_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+
+if(MSVC_VERSION)
+  set(ep_common_C_FLAGS "${ep_common_C_FLAGS} /bigobj /MP")
+  set(ep_common_CXX_FLAGS "${ep_common_CXX_FLAGS} /bigobj /MP")
+endif()
+
+set(ep_common_args
+  -DBUILD_TESTING:BOOL=${ep_build_testing}
+  -DCMAKE_INSTALL_PREFIX:PATH=${ep_install_dir}
+  -DBUILD_SHARED_LIBS:BOOL=${ep_build_shared_libs}
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+  -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+  -DCMAKE_C_FLAGS:STRING=${ep_common_C_FLAGS}
+  -DCMAKE_CXX_FLAGS:STRING=${ep_common_CXX_FLAGS}
+  -DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD}
+  -DCMAKE_CXX_EXTENSIONS:STRING=${CMAKE_CXX_EXTENSIONS}
+  -DCMAKE_CXX_STANDARD_REQUIRED:STRING=${CMAKE_CXX_STANDARD_REQUIRED}
+
+  -DCMAKE_CXX_FLAGS_RELEASE:STRING=${CMAKE_CXX_FLAGS_RELEASE}
+  -DCMAKE_SHARED_LINKER_FLAGS_RELEASE:STRING=${CMAKE_SHARED_LINKER_FLAGS_RELEASE}
+  -DCMAKE_EXE_LINKER_FLAGS_RELEASE:STRING=${CMAKE_EXE_LINKER_FLAGS_RELEASE}
+  -DCMAKE_MODULE_LINKER_FLAGS_RELEASE:STRING=${CMAKE_MODULE_LINKER_FLAGS_RELEASE}
+)
+
+# Include external projects
+foreach(p ${external_projects})
+  include(CMakeExternals/${p}.cmake)
+endforeach()
+
+#-----------------------------------------------------------------------------
+# Set superbuild boolean args
+#-----------------------------------------------------------------------------
+
+set(my_cmake_boolean_args
+  WITH_COVERAGE
+  BUILD_TESTING
+  ${MY_PROJECT_NAME}_BUILD_ALL_PLUGINS
+  )
+
+#-----------------------------------------------------------------------------
+# Create the final variable containing superbuild boolean args
+#-----------------------------------------------------------------------------
+
+set(my_superbuild_boolean_args)
+foreach(my_cmake_arg ${my_cmake_boolean_args})
+  list(APPEND my_superbuild_boolean_args -D${my_cmake_arg}:BOOL=${${my_cmake_arg}})
+endforeach()
+
+#-----------------------------------------------------------------------------
+# Project Utilities
+#-----------------------------------------------------------------------------
+
+set(proj ${MY_PROJECT_NAME}-Utilities)
+ExternalProject_Add(${proj}
+  DOWNLOAD_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  DEPENDS ""
+)
+
+#-----------------------------------------------------------------------------
+# Project Configure
+#-----------------------------------------------------------------------------
+
+set(proj ${MY_PROJECT_NAME}-Configure)
+
+ExternalProject_Add(${proj}
+  DOWNLOAD_COMMAND ""
+  CMAKE_GENERATOR ${gen}
+  CMAKE_CACHE_ARGS
+    ${ep_common_args}
+    ${my_superbuild_boolean_args}
+    -D${MY_PROJECT_NAME}_USE_SUPERBUILD:BOOL=OFF
+    -D${MY_PROJECT_NAME}_USE_GPU:BOOL=${${MY_PROJECT_NAME}_USE_GPU}
+    -D${MY_PROJECT_NAME}_CONFIGURED_VIA_SUPERBUILD:BOOL=ON
+    -D${MY_PROJECT_NAME}_SUPERBUILD_BINARY_DIR:PATH=${PROJECT_BINARY_DIR}
+    # This directory will be added to PATH in run bat-scripts
+    -DDEPENDS_RUNTIME_DIRECTORY:PATH=${ep_prefix}/bin
+
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+  BINARY_DIR ${CMAKE_BINARY_DIR}/${MY_PROJECT_NAME}-build
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  DEPENDS
+    ${MY_PROJECT_NAME}-Utilities
+  )
+
+
+#-----------------------------------------------------------------------------
+# Project
+#-----------------------------------------------------------------------------
+
+if(CMAKE_GENERATOR MATCHES ".*Makefiles.*")
+  set(_build_cmd "$(MAKE)")
+else()
+  set(_build_cmd ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR}/${MY_PROJECT_NAME}-build --config ${CMAKE_CFG_INTDIR})
+endif()
+
+# The variable SUPERBUILD_EXCLUDE_${MY_PROJECT_NAME}BUILD_TARGET should be set when submitting to a dashboard
+if(NOT DEFINED SUPERBUILD_EXCLUDE_${MY_PROJECT_NAME}BUILD_TARGET OR NOT SUPERBUILD_EXCLUDE_${MY_PROJECT_NAME}BUILD_TARGET)
+  set(_target_all_option "ALL")
+else()
+  set(_target_all_option "")
+endif()
+
+add_custom_target(${MY_PROJECT_NAME}-build ${_target_all_option}
+  COMMAND ${_build_cmd}
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${MY_PROJECT_NAME}-build
+  DEPENDS ${MY_PROJECT_NAME}-Configure
+  )
+
+#-----------------------------------------------------------------------------
+# Custom target allowing to drive the build of the project itself
+#-----------------------------------------------------------------------------
+
+add_custom_target(${MY_PROJECT_NAME}
+  COMMAND ${_build_cmd}
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${MY_PROJECT_NAME}-build
+)
