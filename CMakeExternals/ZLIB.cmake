00704028589539e29131f38b8c3b00c626be18a1
Sergey Khlutchin <sercxjo@gmail.com>
2017-07-03 12:33:08 +0400

    Windows dirty build fix

--- CMakeExternals/ZLIB.cmake
+++ CMakeExternals/ZLIB.cmake
@@ -8,6 +8,9 @@
   find_package(ZLIB)
 
   if(ZLIB_FOUND)
+    set(${MY_PROJECT_NAME}_ZLIB_INCLUDE_DIR "${ZLIB_INCLUDE_DIR}")
+    set(${MY_PROJECT_NAME}_ZLIB_LIBRARY_RELEASE "${ZLIB_LIBRARY_RELEASE}")
+    set(${MY_PROJECT_NAME}_ZLIB_LIBRARY_DEBUG "${ZLIB_LIBRARY_DEBUG}")
   else(ZLIB_FOUND)
     set(additional_cmake_args )
     if(CTEST_USE_LAUNCHERS)
